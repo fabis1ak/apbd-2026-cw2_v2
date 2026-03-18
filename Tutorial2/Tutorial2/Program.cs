ae32778c1547926b38057b81fd2a2dab5c3b5f2f
fabis1ak1 <122180227+fabis1ak1@users.noreply.github.com>
2026-03-18 18:15:26 +0100

    Adding CalculateMax method

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Feautre1");
 
@@ -13,3 +14,18 @@ Console.WriteLine("Feautre3");
 Console.WriteLine("Feautre4");
 
 Console.WriteLine("Feautre5");
+
+
+ static int CalculateMax(int[] values)
+{
+    
+    int max = values[0];
+    for (int i = 1; i < values.Length; i++)
+    {
+        if (values[i] > max)
+        {
+            max = values[i];
+        }
+    }
+    return max;
+}
